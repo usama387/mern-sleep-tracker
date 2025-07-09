4dab6d3643b441069cb57ef4a913dd21cebe6632
usama387 <usamarazaaq3@gmail.com>
2025-07-09 12:07:16 +0500

    frontend initial app setup done with installation of React, tailwind and Shadcn Ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div className='font-semibold text-yellow-500'>
+      <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default App
