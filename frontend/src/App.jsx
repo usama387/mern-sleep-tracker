22438bd67d4424c32ffc8e9836376db463d3ff18
usama387 <usamarazaaq3@gmail.com>
2025-07-12 19:30:01 +0500

    Hidden Navbar in doctor panel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./reusable/_components/Navbar";
 import FooterSection from "./reusable/_components/Footer";
@@ -9,20 +9,28 @@ import { useSelector } from "react-redux";
 import SignupPage from "./pages/SignUp";
 import SigninPage from "./pages/SignIn";
 import DashboardPage from "./pages/Dashboard";
+import DoctorPanel from "./pages/DoctorPanel";
 
 const App = () => {
   // state from redux to access user information for authentication
   const { user } = useSelector((store) => store?.auth);
+  
+  // Get the current location
+  const location = useLocation();
+
+  // Conditionally render Navbar (exclude for /doctor-panel)
+  const showNavbar = location.pathname !== "/doctor-panel";
 
   return (
     <div className="">
-      <Navbar />
+      {showNavbar && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/sign-up" element={<SignupPage />} />
         <Route path="/sign-in" element={<SigninPage />} />
+        <Route path="/doctor-panel" element={<DoctorPanel />} />
         <Route
           path="/dashboard"
           element={
